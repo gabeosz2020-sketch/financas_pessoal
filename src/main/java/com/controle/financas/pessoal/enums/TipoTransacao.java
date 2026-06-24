6e487064f00763a816b30da5c3273c27b5d26b8f
Gabriel oliveira <gabeosz2020@gmail.com>
2026-06-23 21:49:30 -0300

    fix: adiciona desserializacao case insensitive para TipoTransacao

--- src/main/java/com/controle/financas/pessoal/enums/TipoTransacao.java
+++ src/main/java/com/controle/financas/pessoal/enums/TipoTransacao.java
@@ -1,8 +1,15 @@
 package com.controle.financas.pessoal.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum TipoTransacao {
     RECEITA,
     DESPESA;
+
+    @JsonCreator
+    public static TipoTransacao fromString(String valor){
+        return TipoTransacao.valueOf(valor.toUpperCase());
+    }
 
     public boolean isReceita(){
         return this == RECEITA;
